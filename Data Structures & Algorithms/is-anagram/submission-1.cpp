f289352e293d6a4409a1896f5f0c631630416e0d
Shibli Sadik Sifat <sadik38003@gmail.com>
2026-07-30 01:22:32 +0600

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq[26]={0};
+
+        for(auto x:s){
+            freq[x-'a']++;
+        }
+        for(auto x:t){
+            freq[x-'a']--;
+        }
+
+        for(auto x:freq){
+            if(x!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
