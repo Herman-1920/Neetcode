6e252e7ed4495c4dbaef8bd6392b7ca39b7aa997
Shibli Sadik Sifat <sadik38003@gmail.com>
2026-08-10 21:05:35 +0600

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count=0
+
+        for i in details:
+            d=i[11]+i[12]
+            if(int(d)>60):count+=1
+
+        return count
